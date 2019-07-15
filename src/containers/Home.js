1433864d80ee0bfc93a572966f619f78aa0935b6
unknown <mariofph@gmail.com.com>
2019-07-15 00:24:38 -0600

    login-with-aws-cognito

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Scratch</h1>
+          <p>Una pagina simple para crear y compartir notas</p>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () =>
+  <div className="NotFound">
+    <h3>Sorry, page not found!</h3>
+    <hr>
+    </hr>
+    <h3>Lo sentimos, pagina no encontrada!</h3>
+  </div>;
