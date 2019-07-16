32e2ba9f77d1d396a52740f45283bd679d6bf299
unknown <mariofph@gmail.com.com>
2019-07-15 19:37:36 -0600

    add-the-create-note-page

--- src/Routes.js
+++ src/Routes.js
@@ -6,11 +6,14 @@ import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 import ResetPassword from "./containers/ResetPassword";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import NewNote from "./containers/NewNote";
+
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <UnauthenticatedRoute
     path="/login/reset"
     exact
--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "note-app-bucket"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://qpl8ajrha8.execute-api.us-east-1.amazonaws.com/prod/"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_rzGAEcDrB",
-      APP_CLIENT_ID: "6ucgtchl9h3g3is6ph13n046n",
-      IDENTITY_POOL_ID: "us-east-1:afb4bcb2-c6c5-4629-b798-b717371906ef"
-    }
-  };
-  
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "note-app-bucket"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://qpl8ajrha8.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_rzGAEcDrB",
+    APP_CLIENT_ID: "6ucgtchl9h3g3is6ph13n046n",
+    IDENTITY_POOL_ID: "us-east-1:afb4bcb2-c6c5-4629-b798-b717371906ef"
+  }
+};
